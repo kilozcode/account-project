1eb7c5074240a30918ea5146639271e13906d863
kilozcode <kayodezollner@hotmail.com>
2018-11-13 19:15:13 +0000

    Testing

--- src/test/java/com/qa/testing/Testing.java
+++ src/test/java/com/qa/testing/Testing.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.qa.app.Service;
+import com.qa.app.account;
+import com.qa.app.person_json;
 
 
 public class Testing {
@@ -18,7 +21,51 @@ public class Testing {
 		service.addnewaccount("joe", "blogs", 5);
 		validUser userCheck = new validUser(service.retrieveMap());
 		assertEquals(2,userCheck.FirstNameCount("joe"));
+		Service s = new Service();
+		s.addnewaccount("scott", "summers", 1);
+		account a = s.findaccount(1);
+		assertEquals("scott", a.getFirstName());
+
+		
+		
 		
 	}
+	@Test
+	public void test2() {
+		account newAcc = new account ();
+		newAcc.setSurname("black");
+		assertEquals("black", newAcc.getSurname());
+		
+	}
+	@Test
+	public void test3() {
+		account newAcc = new account ();
+		newAcc.setFirstName("joe");
+		assertEquals("joe", newAcc.getFirstName());
+		
 
 }
+	
+	@Test
+	public void test4() {
+		account a = new account() ;
+		a.setFirstName("billy");
+		a.setSurname("bob");
+		a.setAccountNumber(1);
+		String output = "";
+		try {
+			output = person_json.convertPerson(a);
+		}
+		catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		assertEquals("{\r\n" + 
+				"  \"firstName\" : \"billy\",\r\n" + 
+				"  \"surname\" : \"bob\",\r\n" + 
+				"  \"accountNumber\" : 1\r\n" + 
+				"}",output);
+	
+	
+			}
+		}
+	
